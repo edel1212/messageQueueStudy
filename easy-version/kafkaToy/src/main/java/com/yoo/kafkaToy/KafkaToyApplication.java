d4baaa2f1403d61c0a50bb04c81276bc67baaa96
jeoungho.yoo <jeoungho.yoo@stans.co.kr>
2024-06-24 18:16:43 +0900

    project init

---
+++ easy-version/kafkaToy/src/main/java/com/yoo/kafkaToy/KafkaToyApplication.java
@@ -0,0 +1,13 @@
+package com.yoo.kafkaToy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaToyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaToyApplication.class, args);
+	}
+
+}
