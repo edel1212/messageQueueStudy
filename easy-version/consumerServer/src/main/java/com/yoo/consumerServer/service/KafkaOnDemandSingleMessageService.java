36b446c71a671111f8c0afec9415756847a79356
jeoungho.yoo <jeoungho.yoo@stans.co.kr>
2024-07-01 10:54:00 +0900

    Type Safety하도록 코드 수정

--- easy-version/consumerServer/src/main/java/com/yoo/consumerServer/service/KafkaOnDemandSingleMessageService.java
+++ easy-version/consumerServer/src/main/java/com/yoo/consumerServer/service/KafkaOnDemandSingleMessageService.java
@@ -1,11 +1,9 @@
 package com.yoo.consumerServer.service;
 
 import lombok.extern.log4j.Log4j2;
-import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
-import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.stereotype.Service;
 
 import java.time.Duration;
@@ -14,13 +12,15 @@ import java.util.Properties;
 
 @Service
 @Log4j2
-public class KafkaOnDemandService {
+public class KafkaOnDemandSingleMessageService {
     public void onDemandMessage(){
         Properties props = new Properties();
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-        props.put(ConsumerConfig.GROUP_ID_CONFIG, "abc");
-        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put("bootstrap.servers", "localhost:9092");
+        props.put("group.id", "abc");
+        props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        // Disable automatic offset commit
+        props.put("enable.auto.commit", false);
 
         try (KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props)) {
             consumer.subscribe(Collections.singletonList("foo"));
@@ -31,11 +31,7 @@ public class KafkaOnDemandService {
                 log.info("---------------------");
                 log.info(record);
                 log.info("---------------------");
-            } // for
-
-        } catch (Exception e){
-            e.printStackTrace();
-            // TODO Exception 처리
-        }// try - catch
+            }
+        }// try
     }
 }
