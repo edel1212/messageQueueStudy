ce73beee66ebb14a5cc5e2bfb4148f0ecae10197
yoo <edel1212@naver.com>
2026-04-06 16:48:19 +0900

    Kafka 잘못된 import 및 @Bean 설정 제거

--- used-KRaft/single-node/kafka-single-node-producer-server/src/main/java/com/yoo/kafkasinglenode/config/KafkaProducerConfig.java
+++ used-KRaft/single-node/kafka-single-node-producer-server/src/main/java/com/yoo/kafkasinglenode/config/KafkaProducerConfig.java
@@ -3,13 +3,13 @@ package com.yoo.kafkasinglenode.config;
 import com.yoo.kafkasinglenode.api.order.dto.OrderCreatedDto;
 import com.yoo.kafkasinglenode.api.payment.dto.PaymentRequestDto;
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.serializer.JacksonJsonSerializer;
-import tools.jackson.databind.ser.jdk.StringSerializer;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -20,7 +20,6 @@ public class KafkaProducerConfig {
     @Value("${spring.kafka.bootstrap-servers}")
     private String bootstrapServers;
 
-    @Bean
     private Map<String, Object> baseConfig() {
 
         Map<String, Object> config = new HashMap<>();
