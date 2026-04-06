a0d6548dad015755ddbec9cbbab9ab09c10b0a0b
yoo <edel1212@naver.com>
2026-04-06 17:43:35 +0900

    consumer server add

---
+++ used-KRaft/single-node/kafka-single-node-cosumer-server/src/main/java/com/yoo/kafkasinglenodecosumerserver/KafkasinglenodecosumerserverApplication.java
@@ -0,0 +1,13 @@
+package com.yoo.kafkasinglenodecosumerserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkasinglenodecosumerserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkasinglenodecosumerserverApplication.class, args);
+	}
+
+}
---
+++ used-KRaft/single-node/kafka-single-node-cosumer-server/src/test/java/com/yoo/kafkasinglenodecosumerserver/KafkasinglenodecosumerserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yoo.kafkasinglenodecosumerserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkasinglenodecosumerserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
