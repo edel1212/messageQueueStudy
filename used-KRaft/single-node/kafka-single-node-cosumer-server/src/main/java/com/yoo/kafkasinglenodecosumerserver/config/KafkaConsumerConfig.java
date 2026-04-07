5b7a1d9b35b4ec3355bfa090017a1be7b72caec6
yoo <edel1212@naver.com>
2026-04-07 16:52:43 +0900

    Consumer 공통 설정을 외부로 빼서 사용하는 구조로 변경

--- used-KRaft/single-node/kafka-single-node-cosumer-server/src/main/java/com/yoo/kafkasinglenodecosumerserver/config/KafkaConsumerConfig.java
+++ used-KRaft/single-node/kafka-single-node-cosumer-server/src/main/java/com/yoo/kafkasinglenodecosumerserver/config/KafkaConsumerConfig.java
@@ -22,24 +22,27 @@ public class KafkaConsumerConfig {
     @Value("${spring.kafka.bootstrap-servers}")
     private String bootstrapServers;
 
-    @Bean
-    public ConsumerFactory<String, PaymentRequestDto> paymentConsumerFactory() {
+    // 공통 설정
+    private Map<String, Object> commonConfig() {
         Map<String, Object> config = new HashMap<>();
-
+        // 브로커 서버 지정
         config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         // "초기 오프셋(Offset) 위치를 가장 처음(가장 오래된 데이터)으로 설정"
         // 과거 유실 데이터 방지
         config.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
         // ✅ 자동 커밋 OFF
         config.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false);
-
         // 💡 역직렬화 세팅 (String -> 객체 변환)
         config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JacksonJsonDeserializer.class);
-
         // 💡 핵심: 어떤 패키지의 DTO로 역직렬화할지 허용 (보안)
         config.put(JacksonJsonDeserializer.TRUSTED_PACKAGES, "*");
+        return config;
+    }
 
+    @Bean
+    public ConsumerFactory<String, PaymentRequestDto> paymentConsumerFactory() {
+        Map<String, Object> config = commonConfig();
 
         // ✅ Producer 타입 헤더 무시하고 Consumer DTO로 강제 매핑
         config.put(JacksonJsonDeserializer.USE_TYPE_INFO_HEADERS, false);
@@ -55,11 +58,13 @@ public class KafkaConsumerConfig {
 
         // 실무 팁: 파티션 수에 맞춰 쓰레드를 늘리면 동시 처리량이 늘어납니다.
         // factory.setConcurrency(3);
-        
+        // 예외 처리
+        // factory.setCommonErrorHandler(paymentErrorHandler());
+
         // 커밋의 제어건을 SpringBoot에 넘김
-        factory.getContainerProperties()
-                .setAckMode(ContainerProperties.AckMode.MANUAL_IMMEDIATE);
+        factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.MANUAL_IMMEDIATE);
 
         return factory;
     }
+
 }
