fbdb271c9111d2041d75a18d527e1678fb9ee21c
yoo <edel1212@naver.com>
2026-04-13 17:26:06 +0900

    잘못된 Topic 명 수정

--- used-KRaft/single-node/kafka-single-node-producer-server/src/main/java/com/yoo/kafkasinglenode/api/order/service/OrderProducerService.java
+++ used-KRaft/single-node/kafka-single-node-producer-server/src/main/java/com/yoo/kafkasinglenode/api/order/service/OrderProducerService.java
@@ -3,7 +3,7 @@ package com.yoo.kafkasinglenode.api.order.service;
 import com.yoo.kafkasinglenode.api.order.dto.OrderCreatedDto;
 
 public interface OrderProducerService {
-    String TOPIC = "order.created";
+    String TOPIC = "order.request";
 
     void sendMessage(OrderCreatedDto dto);
 }
